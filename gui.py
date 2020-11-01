5aeb937d539cfd4b16112f86d7d12cf5a7bdd1bf
= <=>
2020-11-02 00:04:13 +0600

    init

---
+++ gui.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+quit = False
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('Line of action')
